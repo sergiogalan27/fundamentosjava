5b76546fc52d7c701c8e6042606a2ef92e5e1932
Sergio Galán <s.galanopris@colegiovirgendeeuropa.edu.es>
2026-09-15 13:23:25 +0200

    depuracion ejercicio

---
+++ depuracion.java
@@ -0,0 +1,28 @@
+package fundamentosjava;
+
+public class depuracion {
+	public class EjemploDebug {
+	    public static void main(String[] args) {
+	 
+	        int edad = 20;
+	        double saldo = 150.75;
+	        String nombre = "Ana";
+	        boolean mayorDeEdad = false;
+	 
+	        System.out.println("Antes: " + nombre + " " + edad + " " + saldo + " " + mayorDeEdad);
+	 
+	        edad = edad + 5;
+	        saldo = saldo + 100.25;
+	        nombre = nombre + " Garcia";
+	        mayorDeEdad = edad >= 18;
+	 
+	        System.out.println("Despues: " + nombre + " " + edad + " " + saldo + " " + mayorDeEdad);
+	 
+	        if (saldo < 300) {
+	            saldo = saldo + 50;
+	        }
+	 
+	        System.out.println("Final: " + saldo);
+	    }
+	}
+}
