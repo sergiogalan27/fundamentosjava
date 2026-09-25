89bc438ba1d4dba0fb83a98ec0bef84bd5e3a70a
Sergio Galán <s.galanopris@colegiovirgendeeuropa.edu.es>
2026-09-25 11:43:00 +0200

    while loop 1

---
+++ WhileLoop1.java
@@ -0,0 +1,22 @@
+package fundamentosjava;
+/**
+ * imagine the following scenario:
+ * In a game, a intelligent monster
+ * need to launch an attack depending on 
+ * the distance between the player 
+ * and itself,
+ * and also its heath state.
+ * 
+ * 
+ * @author sergio
+ *25 sept 2026
+ */
+public class WhileLoop1{
+	
+	public static void main(String[] args) {
+		int distance = 20;
+		int hp = 234;
+		final int MAX_HP= 234234;
+		
+	}
+}
